6e4a8d487de029f2188d6644fad4ae535d0cafac
Mohinish-k053 <mohinish.k053@gmail.com>
2025-04-21 19:25:30 +0530

    task4

--- terraform/scripts/user_data.sh
+++ terraform/scripts/user_data.sh
@@ -4,11 +4,11 @@ apt install docker.io -y
 systemctl start docker
 docker stop strapi || true
 docker rm strapi || true
-docker pull mohinish053/strapi-task4@DOCKER_TAG@
+docker pull mohinish053/strapi-task4:${DOCKER_TAG}
 docker run -d \
     -e APP_KEYS=$(openssl rand -base64 32),$(openssl rand -base64 32),$(openssl rand -base64 32),$(openssl rand -base64 32) \
     -e API_TOKEN_SALT=$(openssl rand -base64 32) \
     -e ADMIN_JWT_SECRET=$(openssl rand -base64 32) \
     -e TRANSFER_TOKEN_SALT=$(openssl rand -base64 32) \
     -e JWT_SECRET=$(openssl rand -base64 32) \
-    -p 1337:1337 mohinish053/strapi-task4@DOCKER_TAG@
+    -p 1337:1337 mohinish053/strapi-task4:${DOCKER_TAG}
